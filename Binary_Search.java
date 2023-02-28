634ee5b73ec3d1f599c2c7334b764e774e1a85c3
shanvijha30 <shanvijha@protonmail.com>
2023-02-28 18:40:46 +0530

    Changes made to binary search file

--- Binary_Search.java
+++ Binary_Search.java
@@ -5,6 +5,7 @@ public static void main(String args[])
 int [] arr={-18,-12,-4,0,2,3,4,15,16,22,45,89}
 int target=22;
 int ans= binarysearch(arr,target);
+System.out.println(ans);
 }
 static int binarysearch(int[] arr, int target)
 {
